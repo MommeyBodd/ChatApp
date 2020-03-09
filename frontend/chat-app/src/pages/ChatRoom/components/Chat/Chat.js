24e102c8dea04cd6ff7c2e9ac26de46c68490c74
Adryian Siarheyeu <adryian.siarheyeu@itechart-group.com>
2020-03-10 01:09:50 +0300

    super primary layout of Chat done (skeleton)

--- frontend/chat-app/src/pages/ChatRoom/components/Chat/Chat.js
+++ frontend/chat-app/src/pages/ChatRoom/components/Chat/Chat.js
@@ -1,15 +1,65 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import PropTypes from "prop-types";
+import "./chat.scss";
+import mockMessages from "./mockMessages";
+import SendIcon from "@material-ui/icons/Send";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
 
 const Chat = ({ messages }) => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <>
       <div className="messages-area">
-        {messages.map(message => (
-          <div>{message}</div>
+        {mockMessages.map((message, index) => (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: message.incoming ? "flex-start" : "flex-end"
+            }}
+          >
+            <div
+              key={index}
+              style={{
+                maxWidth: 200
+              }}
+            >
+              {message.text}
+            </div>
+          </div>
         ))}
       </div>
-    </div>
+      <div className="input-area">
+        <Button onClick={() => console.log(1)} color="primary">
+          <AttachFileIcon />
+        </Button>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Type"
+          multiline
+          fullWidth
+          rowsMax="3"
+          autoFocus
+          // value={value}
+          // onChange={handleChange}
+          variant="outlined"
+        />
+        <Button onClick={() => console.log(1)} color="primary">
+          <SendIcon />
+        </Button>
+      </div>
+    </>
   );
 };
 
---
+++ frontend/chat-app/src/pages/ChatRoom/components/Chat/mockMessages.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    userName: "Adrik",
+    text: "Hi",
+    incoming: false
+  },
+  {
+    userName: "Adrik",
+    text: "Hasi",
+    incoming: true
+  },
+  {
+    userName: "Adrik",
+    text: "Hdi",
+    incoming: false
+  },
+  {
+    userName: "Adrik",
+    text: "qwe",
+    incoming: true
+  },
+  {
+    userName: "Adrik",
+    text: "ewq",
+    incoming: true
+  },
+  {
+    userName: "Adrik",
+    text: "sss",
+    incoming: true
+  },
+  {
+    userName: "Adrik",
+    text: "Haaaai",
+    incoming: false
+  },
+  {
+    userName: "Adrik",
+    text: "dddd",
+    incoming: false
+  },
+  {
+    userName: "Adrik",
+    text: "Hasdadi",
+    incoming: true
+  }
+];
